6cbbf67182d89db1f28eaad4969c29376de49301
Lia Pires <liasth.pires@gmail.com>
2022-06-25 17:29:52 -0300

    Create ALIASING TABLE NAMES
    Create ALIASING TABLE NAMES

---
+++ Basic Syntax& Queerying Data/ALIASING TABLE NAMES.sql
@@ -0,0 +1,10 @@
+-- ALIASING TABLE NAMES
+
+
+-- Example 18
+-- SELECT departments.departments_id, departments.department_name
+-- FROM departments;
+
+-- Example 19
+SELECT d.departments_id, d.department_name
+FROM departments d;
