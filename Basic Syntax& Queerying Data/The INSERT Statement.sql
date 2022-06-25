c9149677d5326047ce050e4f47e37474063e1f82
Lia Pires <liasth.pires@gmail.com>
2022-06-25 16:36:18 -0300

    Create the INSERT Statement
    Create the INSERT Statement

---
+++ Basic Syntax& Queerying Data/The INSERT Statement.sql
@@ -0,0 +1,22 @@
+-- The INSERT Statement
+
+USE sqlclass_db;
+
+-- Example 8 (Adicionando um único record)
+
+-- INSERT INTO countries
+-- VALUES ('AF', 'Afghanistan', 1001); 
+
+-- Example 9
+-- INSERT INTO countries (country_id, country_name, region_id)
+-- VALUES ('NZ', 'New Zealand', 9009);
+
+-- Example 10
+INSERT INTO countries (country_id, country_name, region_id)
+VALUES ('AL', 'Albania', 2189);
+
+INSERT INTO countries (country_id, country_name, region_id)
+VALUES ('DZ', 'Algeria', 2420);
+
+
+
