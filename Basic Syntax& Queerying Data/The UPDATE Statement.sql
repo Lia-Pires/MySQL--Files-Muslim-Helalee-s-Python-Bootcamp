089aafa3845ce6f805643b780d11ca93214ac82a
Lia Pires <liasth.pires@gmail.com>
2022-06-25 16:56:35 -0300

    Create the UPDATE Statement
    Create the UPDATE Statement

---
+++ Basic Syntax& Queerying Data/The UPDATE Statement.sql
@@ -0,0 +1,18 @@
+-- The UPDATE Statement
+
+
+-- Example 11
+-- UPDATE departments
+-- SET department_name = 'Web Development', location_id = 9009 
+-- WHERE department_id = 1;
+
+
+-- Example 12
+-- UPDATE locations
+-- SET city = 'New York'
+-- WHERE country_id = 'US';
+
+
+-- Example 13 (Estraga as tabelas - muda tudo)
+-- UPDATE locations
+-- SET city = 'New York';
