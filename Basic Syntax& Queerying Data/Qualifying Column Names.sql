9d0d6f7c57a397758bcf4f8637afe6742e55cad2
Lia Pires <liasth.pires@gmail.com>
2022-06-25 17:09:55 -0300

    Create Qualifying Column Names
    Create Qualifying Column Names

---
+++ Basic Syntax& Queerying Data/Qualifying Column Names.sql
@@ -0,0 +1,15 @@
+-- Qualifying Column Names
+
+USE sqlclass_db;
+
+-- Example 15
+-- SELECT country_id, country_name
+-- FROM countries;
+
+-- Example 16
+-- SELECT countries.country_id, countries.country_name
+-- FROM countries;
+
+-- Example 17
+SELECT *
+FROM sqlclass_db.countries;
