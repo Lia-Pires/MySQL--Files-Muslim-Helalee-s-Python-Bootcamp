c3a044e841bea28b4caf0b22e3590cb1349bcb89
Lia Pires <liasth.pires@gmail.com>
2022-06-25 16:13:20 -0300

    Create The SELECT DISTINCT  Statement
    Create The SELECT DISTINCT  Statement

---
+++ Basic Syntax& Queerying Data/The SELECT DISTINCT Statement.sql
@@ -0,0 +1,18 @@
+-- The SELECT DISTINCT Statement
+
+USE sqlclass_db;
+
+-- Example 4
+-- SELECT DISTINCT department_id
+-- FROM employees;
+
+
+-- Example 5
+-- SELECT DISTINCT first_name, last_name, manager_id
+-- FROM employees;
+
+-- Example 6
+SELECT DISTINCT *
+FROM employees;
+
+
