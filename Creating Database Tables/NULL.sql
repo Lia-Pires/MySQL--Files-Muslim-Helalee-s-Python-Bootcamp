f184ef727c2c819156b7a5f99921e6c7a0f13edc
Lia Pires <liasth.pires@gmail.com>
2022-06-26 11:10:28 -0300

    Create NULL
    Create NULL

---
+++ Creating Database Tables/NULL.sql
@@ -0,0 +1,12 @@
+-- NULL 
+
+USE sql_course;
+
+-- Example 57
+CREATE TABLE students 
+(
+student_id INTEGER,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL
+);
+-- ----------------------------
