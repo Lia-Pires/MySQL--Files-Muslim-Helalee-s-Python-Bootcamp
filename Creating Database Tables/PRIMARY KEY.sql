6db8906c9cd4db7432a0f4f2681fb76a614813bd
Lia Pires <liasth.pires@gmail.com>
2022-06-26 11:13:24 -0300

    Create PRIMARY KEY
    Create PRIMARY KEY

---
+++ Creating Database Tables/PRIMARY KEY.sql
@@ -0,0 +1,11 @@
+-- PRIMARY KEY 
+
+USE sql_course;
+
+-- Example 58
+CREATE TABLE students (
+student_id INTEGER PRIMARY KEY,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL
+);
+-- ----------------------------
